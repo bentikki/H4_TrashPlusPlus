eed3ffff2f5f1a0636b371a8098645bd9bf7f36b
Casper Koksby Møller Nielsen <casp8660@zbc.dk>
2021-06-14 09:31:31 +0200

    2- Structur for encryption lib, test for async

---
+++ EncryptionLibrary/Async/IAsyncEncryption.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EncryptionLibrary
+{
+    public interface IAsyncEncryption : IEncryption
+    {
+        byte[] GetPublicKey();
+        void SetPublicKey(byte[] key);
+    }
+}
