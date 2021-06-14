92fc9f98a2dd6d5963d5540c5bfdac8b1083a0ee
benjatikki <benjamin.tikki@gmail.com>
2021-06-14 08:55:53 +0200

    2- Initial Commit with prototype

---
+++ H4_TrashPlusPlus/Helpers/AppSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace H4_TrashPlusPlus.Helpers
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
